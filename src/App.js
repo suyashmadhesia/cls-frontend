30da72fbb6f185e789ea1abd16ef92bfb92c7f40
Suyash <suyashmadhesia@gmail.com>
2022-08-21 17:01:46 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -7,16 +7,48 @@ function App() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState("");
+  const [className, setClassName] = useState("");
+  const [classid, setClassid] = useState("");
+  const [cls, setClass] = useState([]);
   const [register, setRegister] = useState(false);
 
   const handleChange = (e) => {
     setUsername(e.target.value);
   }
 
+  function handleClassIdChange(e){
+    setClassid(e.target.value);
+  }
+
+  const handleClassNameChange = (e) => {
+    setClassName(e.target.value);
+  }
+
   const handleChangeEmail = e => setEmail(e.target.value)
 
   const handleChangePassword = (e) => {
     setPassword(e.target.value);
+  }
+
+  const getAllClasses = () => {
+    if (localStorage.getItem('token')) {
+      fetch(`${URL}/api/u/${localStorage.getItem('account_id')}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${localStorage.getItem('token')}`
+        }
+      }).then(async (res) => {
+        let data = await res.json()
+        if (res.status === 200) {
+          setClass(data.data)
+          console.log(data.data)
+        }
+        else {
+          alert(data.error)
+        }
+      })
+    }
   }
 
   const handleRegister = () => {
@@ -40,12 +72,39 @@ function App() {
           setData(data)
           localStorage.setItem('token', data.token)
           localStorage.setItem('account_id', data.account_id)
+          getAllClasses()
         }
         else {
           alert(data.error)
         }
       }).catch(e => console.error(e))
+      
     }
+  }
+
+  const createClass = () => {
+    if (!className) {
+      alert("Empty Class Name")
+      return;
+    }
+    fetch(`${URL}/api/create-class`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${localStorage.getItem('token')}`
+      },
+      body: JSON.stringify({ name: className })
+    }).then(async res => {
+      setClassName("")
+      let data = await res.json()
+      if (res.status === 201) {
+        getAllClasses()
+        alert(data.message)
+      }
+      else {
+        alert(data.error)
+      }
+    })
   }
 
   const handleLogin = () => {
@@ -70,13 +129,15 @@ function App() {
         setData(data)
         localStorage.setItem('token', data.token)
         localStorage.setItem('account_id', data.account_id)
+        getAllClasses()
       }
       else {
         alert(data.error)
       }
     }).catch(e => console.error(e))
   }
-  useEffect(() => {
+
+  const login = () => {
     if (localStorage.getItem('token')) {
       fetch(`${URL}/api/login`, {
         method: 'POST',
@@ -89,25 +150,62 @@ function App() {
         if (res.status === 200) {
           setData(data)
           console.log(data)
-          localStorage.setItem('token', data.token)
-          localStorage.setItem('account_id', data.account_id)
         }
         else {
           alert(data.error)
         }
       }).catch(e => console.error(e))
+      console.log("Logged in")
     }
+  }
 
+  useEffect(() => {
+    login()
+    getAllClasses()
   }, [])
+
+
+
+
   if (localStorage.getItem('token')) {
-    return <div>
+    return <div className="m-4">
       <div>Welcome: <span className="text-green-500">{data.name}</span></div>
       <div>Your account_id: <span className="text-green-500">{data.account_id}</span></div>
       <div>Your email: <span className="text-green-500">{data.email}</span></div>
+      <div>You are {data.is_faculty ? '' : 'not'} faculty member </div>
+      <div className="mt-4">
+        <div>
+          Your Classrooms
+        </div>
+        <div>
+          {cls.length ? cls.map((item, index) => {
+            return <div className="mt-2" key={index}>
+              <div className="text-green-500">{index + 1}. Class code ==== {item.cls_id}</div>
+              <div className="text-green-500">Class name ==== {item.name}</div>
+              <div className="text-red-500"> Share this code to student for join them in class</div>
+            </div>
+          }) : <span className="text-red-400">
+            {data.is_faculty ? "No Classroom created yet" : "No Classroom joined yet"}
+          </span>}
+        </div>
+      </div>
+      {data.is_faculty ?
+
+        <div className="mt-4">
+          <input className="rounded-md bg-red-100 border-black my-4" type="text" value={className} onChange={handleClassNameChange} placeholder="Class Name" /><br />
+          <button className="rounded-lg py-2 bg-red-400 px-5 border-black" type="submit" onClick={()=> createClass()}>Create Class</button>
+        </div> : <div className="mt-4">
+          <input className="rounded-md bg-red-100 border-black my-4" type="text" onChange={handleClassIdChange} value={classid} placeholder="Class id" /><br />
+          <button className="rounded-lg py-2 bg-red-400 px-5 border-black" type="submit" onClick={()=> {}}>Join Class</button>
+        </div>
+
+
+      }
       <div>
-        <button className="bg-red-400 rounded-lg p-2" onClick={() => {
+        <button className="bg-red-400 rounded-lg px-3 py-2 mt-4" onClick={() => {
           localStorage.clear();
           setData('Welcome');
+          setClass([]);
         }}>Logout</button>
       </div>
     </div>
@@ -123,7 +221,7 @@ function App() {
         <div>
           <input className="rounded-md bg-red-100 border-black my-4" type="text" value={username} onChange={handleChange} placeholder="username" /><br />
           <input className="rounded-md bg-red-100 border-black my-2" type="password" value={password} onChange={handleChangePassword} placeholder="password" /><br />
-          <button className="rounded-lg py-2 bg-red-400 px-5 border-black" type="submit" onClick={handleLogin}>Login</button>
+          <button className="rounded-lg py-2 bg-red-400 px-5 border-black" type="submit" onClick={()=>handleLogin()}>Login</button>
         </div></div>}
       {register &&
         <div className="mt-4 mx-4">
@@ -135,7 +233,7 @@ function App() {
             <input className="rounded-md bg-red-100 border-black my-4" type="text" value={username} onChange={handleChange} placeholder="username" /><br />
             <input className="rounded-md bg-red-100 border-black my-2" type="email" value={email} onChange={handleChangeEmail} placeholder="email" /><br />
             <input className="rounded-md bg-red-100 border-black my-2" type="password" value={password} onChange={handleChangePassword} placeholder="password" /><br />
-            <button className="rounded-lg py-2 bg-red-400 px-5 border-black" type="submit" onClick={handleRegister}>Register</button>
+            <button className="rounded-lg py-2 bg-red-400 px-5 border-black" type="submit" onClick={()=>handleRegister()}>Register</button>
           </div>
         </div>
       }
